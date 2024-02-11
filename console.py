21c87b4ccef23d673dfbcc02c054491c1bce5b05
DadMed <mohammed.dad.ens@gmail.com>
2024-02-11 16:40:41 +0100

    all tasks

--- console.py
+++ console.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """
+ Quit command to exit the program.
 """
 import  cmd
 
 class HBNBCommand(cmd.Cmd):
     """
+     Quit command to exit the program.
     """
 
     prompt = "(hbnb)"
     
     def do_quit(self, arg):
         """
+        Quit command to exit the program.
         """
         return True
     
     def help_quit(self, arg):
         """
+         Quit command to exit the program.
         """
         print("Qiut command to exit the program")
     
     def do_EOF(self, arg):
         """
+        EOF (Ctrl+D) signal to exit the program.
         """
         print()
         return True
